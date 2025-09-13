06f46940679f33ddaaf877ed4649c39de3e4dd90
HarryTLG <harry.sibbenga@transforming-learning.com>
2025-09-13 14:05:04 +0100

    ssr changes

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,13 +3,15 @@ import vue from '@astrojs/vue'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
 import partytown from '@astrojs/partytown'
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   site: 'https://harrysibbenga-dev.vercel.app',
 
-  output: 'static', 
+  output: 'server',
 
   integrations: [
+    vercel(),
     vue(),
     tailwind({ applyBaseStyles: false }),
     sitemap(),
